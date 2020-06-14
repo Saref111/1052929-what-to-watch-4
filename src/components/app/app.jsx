9de56aa7e79c143b1e532696f49b4595578e1422
Saref111 <saref012@gmail.com>
2020-06-14 21:19:07 +0300

    1

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,6 +4,7 @@ import Main from "../main/main.jsx";
 const App = (props) => {
   // eslint-disable-next-line react/prop-types
   const {name, genre, year} = props;
+
   return (
     <Main name={name} genre={genre} year={year} />
   );
