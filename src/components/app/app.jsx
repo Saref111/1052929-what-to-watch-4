85238d7586e4a017480ff70cd218103c22d3d904
Saref111 <saref012@gmail.com>
2020-06-14 20:36:41 +0300

    Added propTypes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Main from "../main/main.jsx";
 
 const App = (props) => {
@@ -7,6 +8,12 @@ const App = (props) => {
   return (
     <Main name={name} genre={genre} year={year} />
   );
+};
+
+App.propTypes = {
+  name: PropTypes.string.isRequired,
+  genre: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired,
 };
 
 export default App;
