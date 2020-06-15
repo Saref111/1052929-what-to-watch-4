2156e7e6588480e5af23a76fc1b4b68191f2db7e
Saref111 <saref012@gmail.com>
2020-06-15 16:24:43 +0300

    Created snap tests

---
+++ src/components/app/app.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import App from "./app.jsx";
+
+describe(`Test App`, () => {
+  it(`App snapshot`, () => {
+    const tree = renderer.create(
+        <App
+          name={`The Benders`}
+          genre={`Robcore`}
+          year={3020}
+        />
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+});
---
+++ src/components/main/main.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Main from "./main.jsx";
+
+describe(`Test Main`, () => {
+  it(`Main snapshot`, () => {
+    const tree = renderer.create(
+        <Main
+          name={`The Benders`}
+          genre={`Robcore`}
+          year={3020}
+        />
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+});
