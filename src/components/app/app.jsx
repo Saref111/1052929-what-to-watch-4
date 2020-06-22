52b1a3cb05cd53406758fdb036820172b0385155
Saref111 <saref012@gmail.com>
2020-06-22 20:50:12 +0300

    Added Router to App component/ Remake App from PureComonent

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,18 +1,41 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import Main from "../main/main.jsx";
 
-const App = ({name, genre, year, onHeaderClickHandler, films}) => {
-  return (
-    <Main
-      name={name}
-      genre={genre}
-      year={year}
-      onHeaderClickHandler={onHeaderClickHandler}
-      films={films}
-    />
-  );
-};
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  _renderApp() {
+    const {name, genre, year, onHeaderClickHandler, films} = this.props;
+
+    return (
+      <Main
+        name={name}
+        genre={genre}
+        year={year}
+        onHeaderClickHandler={onHeaderClickHandler}
+        films={films}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {this._renderApp()}
+          </Route>
+          <Route exact path="/dev-component">
+            {/* <Component /> */}
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   name: PropTypes.string.isRequired,
