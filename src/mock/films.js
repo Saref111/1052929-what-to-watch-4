57f102f47bd4e50530e0d60e4823808d9112f881
Saref111 <saref012@gmail.com>
2020-06-25 11:14:39 +0300

    Added markup for movie page and add new data in the mock

--- src/mock/films.js
+++ src/mock/films.js
@@ -1,36 +1,94 @@
 const HREF = `http://dummyimage.com/280x175/D76E00&text=`;
+const BG_HREF= `http://dummyimage.com/&text=`;
+const POSTER_HREF= `http://dummyimage.com/218x327&text=`;
 
 export default [
   {
+    id: 0,
     title: `Джей и молчаливый Боб`,
     src: HREF,
+    details: {
+      bgPoster: BG_HREF,
+      cover: POSTER_HREF,
+      genre: `comedy`,
+      year: 2001,
+    },
   },
   {
+    id: 1,
     title: `Карты, деньги, два ствола`,
     src: HREF,
+    details: {
+      bgPoster: BG_HREF,
+      cover: POSTER_HREF,
+      genre: `comedy`,
+      year: 2001,
+    },
   },
   {
+    id: 2,
     title: `Зеленый слоник`,
     src: HREF,
+    details: {
+      bgPoster: BG_HREF,
+      cover: POSTER_HREF,
+      genre: `comedy`,
+      year: 2001,
+    },
   },
   {
+    id: 3,
     title: `Матрица`,
     src: HREF,
+    details: {
+      bgPoster: BG_HREF,
+      cover: POSTER_HREF,
+      genre: `comedy`,
+      year: 2001,
+    },
   },
   {
+    id: 4,
     title: `Законопослушный гражданин`,
     src: HREF,
+    details: {
+      bgPoster: BG_HREF,
+      cover: POSTER_HREF,
+      genre: `comedy`,
+      year: 2001,
+    },
   },
   {
+    id: 5,
     title: `Человек с Земли`,
     src: HREF,
+    details: {
+      bgPoster: BG_HREF,
+      cover: POSTER_HREF,
+      genre: `comedy`,
+      year: 2001,
+    },
   },
   {
+    id: 6,
     title: `Автострада 60: дорожные истории`,
     src: HREF,
+    details: {
+      bgPoster: BG_HREF,
+      cover: POSTER_HREF,
+      genre: `comedy`,
+      year: 2001,
+    },
   },
   {
+    id: 7,
     title: `Appleman`,
     src: HREF,
+    details: {
+      bgPoster: BG_HREF,
+      cover: POSTER_HREF,
+      genre: `comedy`,
+      year: 2001,
+    },
   },
 ];
