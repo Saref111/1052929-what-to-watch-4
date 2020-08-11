d31bda1ce1ce3f1c8eb6310f5c26981e51324175
Saref111 <saref012@gmail.com>
2020-08-11 13:50:24 +0300

    Added new test

---
+++ src/components/private-route/private-route.test.js
@@ -0,0 +1,48 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import PrivateRoute from "./private-route.jsx";
+import NewReviewPage from "../new-review-page/new-review-page.jsx";
+import { Routes } from "../../const.js";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import { BrowserRouter } from "react-router-dom";
+
+const mockStore = configureStore([]);
+
+describe(`PrivateRoute snaptest`, () => {
+  test(`shouldn't render NewReviewPage`, () => {
+    const store = mockStore({DATA: {
+    },
+    MOVIE: {
+    },
+    USER: {
+      authorizationStatus: `NO_AUTH`,
+      isSigningIn: false
+    }});
+    const tree = renderer.create(<Provider store={store}><BrowserRouter><PrivateRoute exact path={Routes.REVIEW}
+      render={(props) => {
+        return <NewReviewPage {...props}/>;
+      }}
+    /></BrowserRouter></Provider>).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test(`should render NewReviewPage`, () => {
+    const store = mockStore({DATA: {
+    },
+    MOVIE: {
+    },
+    USER: {
+      authorizationStatus: `AUTH`,
+      isSigningIn: false
+    }});
+    const tree = renderer.create(<Provider store={store}><BrowserRouter><PrivateRoute exact path={Routes.REVIEW}
+      render={(props) => {
+        return <NewReviewPage {...props}/>;
+      }}
+    /></BrowserRouter></Provider>).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
